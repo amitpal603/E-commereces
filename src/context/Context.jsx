7ea947b2a9ae82be2b419faf0f3113da68d14222
amitpal603-ops <ap036776@gmail.com>
2025-09-07 21:06:19 +0530

    localStorage add

--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -7,20 +7,19 @@ function Context({ children }) {
   const [product, setProduct] = useState([]);
   const [loading, setLoading] = useState(false);
   const [cart, setCart] = useState([]);
-  const [search,setSearch] = useState("")
-  const [cartDetail,setCartDetail] = useState(null)
+  const [search, setSearch] = useState("");
+  const [cartDetail, setCartDetail] = useState(null);
 
   const fetchData = async () => {
-     
     try {
-      setLoading(true)
+      setLoading(true);
       const response = await fetch("https://dummyjson.com/products");
       const data = await response.json();
 
-      if(data && data.products){
-        setProduct(data.products)
+      if (data && data.products) {
+        setProduct(data.products);
       }
-      setLoading(false)
+      setLoading(false);
     } catch (error) {
       console.log(error);
     }
@@ -28,74 +27,93 @@ function Context({ children }) {
 
   useEffect(() => {
     fetchData();
-    // setCart(JSON.parse(localStorage.getItem('cartItem') || []))
+    const save = JSON.parse(localStorage.getItem("cartItem") || []);
+    setCart(save);
   }, []);
 
-  // search feature add 
+  // search feature add
 
   const filterSearch = product.filter((item) => {
-    const searchTerm = search.toLowerCase()
+    const searchTerm = search.toLowerCase();
 
-    if(searchTerm === "") return true
+    if (searchTerm === "") return true;
 
     return (
       item.title.toLowerCase().includes(searchTerm) ||
       item.category.toLowerCase().includes(searchTerm)
-    )
-  })
+    );
+  });
   // add to cart feature add
 
   const HandleAddToCart = (singleProduct) => {
-    const existingProduct = [...cart]
-     const findIndexCurrentItem  = existingProduct.findIndex((cart) => cart.id === singleProduct.id)  
-    
-     if(findIndexCurrentItem === -1){
+    const existingProduct = [...cart];
+    const findIndexCurrentItem = existingProduct.findIndex(
+      (cart) => cart.id === singleProduct.id
+    );
+
+    if (findIndexCurrentItem === -1) {
       existingProduct.push({
         ...singleProduct,
         quantity: 1,
-        totalPrice : singleProduct?.price
-      })
-      toast.success('Add to cart successFully')
-     }
-     else {
+        totalPrice: singleProduct?.price,
+      });
+      toast.success("Add to cart successFully");
+    } else {
       existingProduct[findIndexCurrentItem] = {
         ...existingProduct[findIndexCurrentItem],
         quantity: existingProduct[findIndexCurrentItem].quantity + 1,
-        totalPrice : (existingProduct[findIndexCurrentItem].quantity + 1) * existingProduct[findIndexCurrentItem].price 
-      }
-      
-     }
-    setCart(existingProduct)
-    // localStorage.setItem('cartItem',JSON.stringify(existingProduct))
-  }
+        totalPrice:
+          (existingProduct[findIndexCurrentItem].quantity + 1) *
+          existingProduct[findIndexCurrentItem].price,
+      };
+    }
+    setCart(existingProduct);
+   if(typeof window !== 'undefine'){
+      localStorage.setItem('cartItem',JSON.stringify(existingProduct))
+   }
+  };
   // remove cart
 
-  const HandleRemove = (getCartItem,isFullyRemove)  => {
-    const existingItem = [...cart]
-    const findIndex = existingItem.findIndex((item) => item.id === getCartItem.id)
+  const HandleRemove = (getCartItem, isFullyRemove) => {
+    const existingItem = [...cart];
+    const findIndex = existingItem.findIndex(
+      (item) => item.id === getCartItem.id
+    );
 
-    if(isFullyRemove){
-      existingItem.splice(findIndex,1)
-      toast.error('Remove Item into cart')
-    }
-    else{
+    if (isFullyRemove) {
+      existingItem.splice(findIndex, 1);
+      toast.error("Remove Item into cart");
+    } else {
       existingItem[findIndex] = {
         ...existingItem[findIndex],
-        quantity :existingItem[findIndex].quantity - 1,
-        totalPrice : (existingItem[findIndex].quantity -1) * existingItem[findIndex].price
-      }
+        quantity: existingItem[findIndex].quantity - 1,
+        totalPrice:
+          (existingItem[findIndex].quantity - 1) *
+          existingItem[findIndex].price,
+      };
     }
-    // localStorage.setItem('cartItem',JSON.stringify(existingItem))
-    // setCart(existingItem)
-  }
-   const removeHandle = (id) => {
-    const update = cart.filter((item) => item.id !== id)
-    localStorage.setItem("cartItem",JSON.stringify(update) || [])
-    setCart(update)
-
-   }
+    // if(typeof window !== 'undefined')
+    // {
+    //   localStorage.setItem('cartItem',JSON.stringify(existingProduct))
+    // }
+    setCart(existingItem)
+  };
+  const removeHandle = (id) => {
+    const update = cart.filter((item) => item.id !== id);
+    localStorage.setItem("cartItem", JSON.stringify(update) || []);
+    setCart(update);
+  };
   const Store = {
-    product ,loading,setSearch,filterSearch,cartDetail,setCartDetail,setLoading,HandleAddToCart,cart,HandleRemove
+    product,
+    loading,
+    setSearch,
+    filterSearch,
+    cartDetail,
+    setCartDetail,
+    setLoading,
+    HandleAddToCart,
+    cart,
+    HandleRemove,
   };
   return <Ecommerce.Provider value={Store}>{children}</Ecommerce.Provider>;
 }
